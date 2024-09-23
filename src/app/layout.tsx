ec2d22d6b98736a06c32786b1fdf226b578eb373
JuHeonHa <hjhjaueon97@gmail.com>
2024-09-23 13:39:53 +0900

    feat: data-memoization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,24 @@ import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
 
+async function Footer() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+  );
+  if (!response.ok) {
+    return <footer>Developed By Neon</footer>;
+  }
+  const books = await response.json();
+  const bookCount = books.length;
+
+  return (
+    <footer>
+      <div>Developed By Neon</div>
+      <div>{bookCount}개의 도서가 등록되어 있습니다.</div>
+    </footer>
+  );
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -15,7 +33,7 @@ export default function RootLayout({
             <Link href={"/"}>📚 BOOK STORE</Link>
           </header>
           <main>{children}</main>
-          <footer>Developed by Neon</footer>
+          <Footer />
         </div>
       </body>
     </html>
