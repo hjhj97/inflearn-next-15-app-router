aceac178f48a42814c171f8f56c92b9f8d423c67
JuHeonHa <hjhjaueon97@gmail.com>
2024-09-25 13:44:47 +0900

    feat: servar action in client

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -10,7 +10,8 @@ export function generateStaticParams() {
 
 async function BookDetail({ bookId }: { bookId: string }) {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`,
+    { cache: "force-cache" }
   );
 
   if (!response.ok) {
@@ -44,7 +45,8 @@ async function BookDetail({ bookId }: { bookId: string }) {
 
 async function ReviewList({ bookId }: { bookId: string }) {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}`,
+    { next: { tags: [`review-${bookId}`] } }
   );
   if (!response.ok) {
     throw new Error("Review Fetch Failed : " + response.statusText);
